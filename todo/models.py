3f3d045a1702c5ebe0c19740b9233642942d8432
kennishastephen <kstephe6@sgu.edu>
2025-05-01 22:28:34 -0400

    Update models.py

--- todo/models.py
+++ todo/models.py
@@ -1,12 +1,7 @@
-
-
 from django.db import models
 
 class Task(models.Model):
-    title = models.CharField(max_length=200)
-    completed = models.BooleanField(default=False)
-    created_at = models.DateField(auto_now_add=True)
+    title = models.CharField(max_length=255)
 
     def __str__(self):
         return self.title
-
